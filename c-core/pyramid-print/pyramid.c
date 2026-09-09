737892d3a5975ecda1769b61c82885cc45d2a383
ImTazake <226741084+ImTazake@users.noreply.github.com>
2026-09-09 03:54:12 +0000

    Solve pyramid-print
    Validated on Grademe on 2026-09-09.
    
    - pyramid-print (practice)
    
    Every exercise here passed the Grademe grader: https://grademe.io
    
    Co-authored-by: Grademe <320286061+grademe-sync[bot]@users.noreply.github.com>

---
+++ c-core/pyramid-print/pyramid.c
@@ -0,0 +1,40 @@
+#include <stdlib.h>
+#include <unistd.h>
+
+void	print_pyramid(int n)
+{
+{
+	int	i;
+	int	j;
+
+	i = 1;
+	while (i <= n)
+	{
+		j = 0;
+		while (j < n - i)
+		{
+			write(1, " ", 1);
+			j++;
+		}
+		j = 0;
+		while (j < (2 * i) - 1)
+		{
+			write(1, "#", 1);
+			j++;
+		}
+		write(1, "\n", 1);
+		i++;
+	}
+}
+}
+
+int	main(int ac, char **av)
+{
+	if(ac != 2)
+	{
+		write(1, "wrong number of arguments\n", 26);
+		return (0);
+	}
+	print_pyramid(atoi(av[1]));
+	return (0);
+}
