1f4526b25a15b2e58d77afa0f4dddb8e6f56d1fd
ImTazake <226741084+ImTazake@users.noreply.github.com>
2026-09-14 22:15:23 +0000

    Solve flip-case
    Validated on Grademe on 2026-09-14.
    
    - flip-case (practice)
    
    Every exercise here passed the Grademe grader: https://grademe.io
    
    Co-authored-by: Grademe <320286061+grademe-sync[bot]@users.noreply.github.com>

---
+++ c-core/flip-case/flip_case.c
@@ -0,0 +1,34 @@
+#include <unistd.h>
+
+void	flip_case(char *str)
+{
+	int i = 0;
+	while (str[i])
+	{
+		if (str[i] >= 'a' && str[i] <= 'z')
+		{
+			str[i] -= 32;
+			write(1, &str[i], 1);
+		}
+		else if (str[i] >= 'A' && str[i] <= 'Z')
+		{
+			str[i] += 32;
+			write(1, &str[i], 1);
+		}
+		else
+			write(1, &str[i], 1);
+		i++;
+	}
+}
+
+int	main(int argc, char **argv)
+{
+	if (argc != 2)
+	{
+		write(1, "wrong number of arguments\n", 26);
+		return (0);
+	}
+	flip_case(argv[1]);
+	write(1, "\n", 1);
+	return (0);
+}
